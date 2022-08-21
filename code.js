23046baac4c510c7e0ac531501239511320b2c80
Ivan Rubio <smashedpurse@gmail.com>
2022-08-21 10:10:04 -0500

    Se genera menú de inicio en una pagina así como delimitación de zona de juego

---
+++ code.js
@@ -0,0 +1,3 @@
+//Declaramos y asignamos nuestro Canvas principal así como la dimensión en la cual trabajaremos (x,y)
+let lienzo = document.getElementById("canvas")
+let ctx = lienzo.getContext("2d")
