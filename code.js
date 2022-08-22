ae4285595fada7c326fe1d2a571898a1287ca1d5
Ivan Rubio <smashedpurse@gmail.com>
2022-08-22 18:11:09 -0500

    Se logra generar colision entre objeto en X y Objeto descendiendo de Y

--- code.js
+++ code.js
@@ -3,82 +3,85 @@ let lienzo = document.getElementById("canvasGameSection")
 let ctx = lienzo.getContext("2d")
 
 const Mike= new Image()
-Mike.src = "/Pictures/xp.png"
+Mike.src = "/Pictures/Redman.png"
 
 const Alienred= new Image()
-Alienred.src = "/Pictures/AlienRojo.png"
+Alienred.src = "/Pictures/Alien.Der.png"
 
 
 const Alienblue= new Image()
-Alienblue.src = "/Pictures/Alienazul.png"
+Alienblue.src = "/Pictures/Blue.png"
 
 const AlienAyellow= new Image()
-AlienAyellow.src = "/Pictures/Bomba.png"
+AlienAyellow.src = "/Pictures/47.png"
 
 const gasBomb= new Image()
 gasBomb.src = "/Pictures/Gas.png"
 
-//Dibujar piso
-// function dibujarPiso(){
-//  ctx.beginPath()
-//  ctx.moveTo(0,750)
-//  ctx.lineTo(1918,750)
-//  ctx.stroke();
-//  ctx.closePath()
-// }
-// dibujarPiso()
+const Mineral= new Image()
+Mineral.src = "/Pictures/Mineral.png"
 
+const Note1= new Image()
+Note1.src = "/Pictures/ProyectilDer.png"
+
+const Note2= new Image()
+Note2.src = "/Pictures/Proyectilizq.png"
 
 //LISTA DE enemigos / Otros elementos
 
 const aliensRojo=[];
 const aliensAmarillo=[];
 const alienRojoIzq=[]
-const NotasMusicales=[]
 const gasContainer=[]
+const MineralDeposito=[]
+const ProyectilDerDeposito=[]
+const ProyectilDerDepositoIzq=[]
 
 
 // PERSONAJES ---------------------------------------------------------------------------------
 
 class Astronauta{ 
-    constructor(x,y,w,h,color,vida,imagen){
+    constructor(x,y,w,h,vida,imagen){
         this.x=x
         this.y=y
         this.w=w
         this.h=h
-        this.color=color
         this.vida=vida
         this.imagen=imagen
     }
     avanzar(){
-        console.log("Avanza")
+       
         if(this.x + this.w <1800){
             this.x += 100
         }
     }
     retroceder(){
-        console.log("Retrocede")
+       
         if(this.x >0){
             this.x -= 100
         }
     }
-    brincar(){
-        console.log("Brinca")
-    }
     disparar(){
-        const disparo = new Musica (this.x + this.w, this.y + (this.h/2),30,40)
-        NotasMusicales.push(disparo)
-        console.log(NotasMusicales)
+        const ProyectilDer = new Disparo (this.x + this.w,this.y + (this.h/2),20,40)
+        ProyectilDerDeposito.push(ProyectilDer)
+        console.log(ProyectilDerDeposito)
     }
+
+    dispararizq(){
+        const Proyectilizq = new DisparoIzq (this.x -20, this.y + (this.h/2),20,40)
+        ProyectilDerDeposito.push(Proyectilizq)
+        console.log(ProyectilDerDeposito)
+    }
+    
     morir(){
-        console.log("Tu personaje ha muerto")
     }
+    
     dibujarse(){
+        ctx.fillStyle = "dodgerblue"
         ctx.fillRect(this.x,this.y,this.w,this.h)
         ctx.drawImage(this.imagen, this.x, this.y,this.w,this.h);
     }
 } 
-
 class AlienRojo{
     constructor(x,y,w,h,imagen,){
         this.x=x
@@ -88,13 +91,10 @@ class AlienRojo{
         this.imagen=imagen
     }
     dibujarse(){
-        ctx.fillStyle = "red"
-        ctx.fillRect(this.x,this.y,this.w,this.h)
         ctx.drawImage(this.imagen, this.x, this.y,this.w,this.h);
        this.x -=1
     }
 }
-
 class AlienRojoIzq{
     constructor(x,y,w,h,imagen,){
         this.x=x
@@ -104,9 +104,7 @@ class AlienRojoIzq{
         this.imagen=imagen
     }
     dibujarse(){
-        ctx.fillStyle = "blue"
-        ctx.fillRect(this.x,this.y,this.w,this.h)
-        ctx.drawImage(this.imagen, this.x, this.y,this.w,this.h);
+       ctx.drawImage(this.imagen, this.x, this.y,this.w,this.h);
         this.x +=1
     }
 }
@@ -119,13 +117,12 @@ class AlienAmarillo{
         this.imagen=imagen
     }
     dibujarse(){
-        ctx.fillStyle = "yellow"
+        ctx.fillStyle = "dodgerblue"
         ctx.fillRect(this.x,this.y,this.w,this.h)
         ctx.drawImage(this.imagen, this.x, this.y,this.w,this.h);
-       this.y +=10
+        this.y +=5 
     }
 }
-
 class Gas{
     constructor(x,y,w,h,imagen,){
         this.x=x
@@ -135,57 +132,83 @@ class Gas{
         this.imagen=imagen
     }
     dibujarse(){
-        ctx.fillStyle = "yellow"
-        ctx.fillRect(this.x,this.y,this.w,this.h)
+        
         ctx.drawImage(this.imagen, this.x, this.y,this.w,this.h);
-       this.y +=10
+       this.y +=5
+    }
+}
+class MineralCoin{
+    constructor(x,y,w,h,imagen,){
+        this.x=x
+        this.y=y
+        this.w=w
+        this.h=h
+        this.imagen=imagen
+    }
+    dibujarse(){
+        
+        ctx.drawImage(this.imagen, this.x, this.y,this.w,this.h);
+        this.y +=5
     }
 }
 
-//PROYECIL
-
-class Musica{
+class Disparo{
     constructor(x,y,w,h){
         this.x=x
         this.y=y
         this.w=w
         this.h=h
+        
     }
     dibujarse(){
-        ctx.fillStyle = "dodgerblue"
-        ctx.fillRect(this.x,this.y,this.w,this.h)
-        this.x +=3
+        ctx.fillStyle = "green"
+        ctx.fillRect(this.x, this.y,this.w,this.h)
+        
+        this.x +=5
     }
 }
 
+class DisparoIzq{
+    constructor(x,y,w,h){
+        this.x=x
+        this.y=y
+        this.w=w
+        this.h=h
+        
+    }
+    dibujarse(){
+        ctx.fillStyle = "green"
+        ctx.fillRect(this.x, this.y,this.w,this.h)
+        
+        this.x -=5
+    }
+}
 
 // MOVIMIENTO------------------------------------------------------------------------------------
-
 //Función para identificar el tipo de tecla que se presiona
 function teclas(astro){
     document.addEventListener("keyup", (evento) => {
-        switch(evento.code){
+        // console.log("Tecla tocada",evento.code)
+      switch(evento.code){
             case "ArrowRight":
                 astro.avanzar();
                 break;
             case "ArrowLeft":
                 astro.retroceder()
                 break                                            
-            case "KeyF":
+            case "KeyD":
                     astro.disparar()
                     break
             case "Space":
                     console.log("Brinco")
                     break;
+            case "KeyA":
+                astro.dispararizq()
+                break                   
         }
     });
 }
-
-
-
-
 // Mostar información------------------------------------------------------------------------------
-
 function mostrarDatos(score,vida,Gasolina){ 
     ctx.fillStyle = "black"
     ctx.font = "35px Arial";
@@ -193,111 +216,138 @@ function mostrarDatos(score,vida,Gasolina){
     //Vida
     ctx.fillText(`Vida: ${vida}`,80,35)
     //Puntaje
-    ctx.fillText(`Score:${score} puntos`,80,85)
+    ctx.fillText(`Score ${score} puntos`,80,85)
     //Gasolina
-    ctx.fillText(`Gasolina Total:`,80,135)
+    ctx.fillText(`Gasolina Total: ${Gasolina}`,80,135)
     
 }
 mostrarDatos()
-
-
 // ENEMIGOS----------------------------------------------------------------------------------------
- //Crear enemigos
-
-    function crearAlien(){
+ function crearAlien(){
         const num = Math.floor(Math.random()*100)
         if(num == 3){
         const redAliend = new AlienRojo (1800,710,40,200,Alienred) 
         aliensRojo.push(redAliend)   
-        }
-    }
-
-    function crearAlienIzq(){
+        }}
+function crearAlienIzq(){
         const num = Math.floor(Math.random()*100)
         if(num == 3){
         const redAliendIzq = new AlienRojoIzq (0,710,40,200,Alienblue) 
         alienRojoIzq.push(redAliendIzq)   
-        }
-    }
-
-    function crearAlienAmarillo(){
+        }}
+function crearAlienAmarillo(){
         const num = Math.floor(Math.random()*50)
         if(num == 3){
-        const Amarillo = new AlienAmarillo (Math.floor(Math.random()*1900),0,60,60,AlienAyellow) 
-        aliensAmarillo.push(Amarillo)   
-        }
-    }
-
-    function gasObjective(){
-        const num = Math.floor(Math.random()*200)
+        const Amarillo = new AlienAmarillo (Math.floor(Math.random()*1850),0,40,40,AlienAyellow) 
+        aliensAmarillo.push(Amarillo)  
+        console.log 
+        }}
+function gasObjective(){
+        const num = Math.floor(Math.random()*300)
         if(num == 3){
-        const Amarillo = new AlienAmarillo (Math.floor(Math.random()*1900),0,60,60,gasBomb) 
-        aliensAmarillo.push(Amarillo)   
+        const Gasoline = new Gas (Math.floor(Math.random()*1800),0,60,60,gasBomb) 
+        gasContainer.push(Gasoline)   
+        }
+    }
+function generateCoin(){
+        const num = Math.floor(Math.random()*300)
+        if(num == 3){
+        const Gold = new MineralCoin (Math.floor(Math.random()*1800),0,60,60,Mineral) 
+        MineralDeposito.push(Gold)   
         }
     }
     
-
-function iniciarJuego(){
     
-    //ASTRONAUTA
-    const astro  = new Astronauta(900,710,80,200,"green",100,Mike) 
+function iniciarJuego(){
+    let score=0;
+    let Gasolina=0
+    const astro = new Astronauta(900,710,80,200,100,Mike)
     teclas(astro)
     console.log(astro)
     astro.dibujarse(); 
+    
 
-
- 
-    setInterval(() => {
-        ctx.clearRect(0,0,1918,963)
-        mostrarDatos(astro.score,astro.vida,0)
-        // dibujarPiso()
-        astro.dibujarse()
+setInterval(() => {
         
+        ctx.clearRect(0,0,1918,963)
+        mostrarDatos(score,astro.vida,Gasolina)
+        astro.dibujarse()
+
         // CREAR ENEMIGO
+
         aliensRojo.forEach((Rojo) => {
             Rojo.dibujarse();
+            
             if(Rojo.x <= astro.x + astro.w){
                 aliensRojo.splice(0,1)
                 astro.vida -=25
-                astro.score +=50
-            }
-            if(astro.vida <0){
+                score +=25
+           
+                aliensRojo.splice(0,1)
+
+            }if(astro.vida <0){
                 console.log("El juego terminó")
             }
 
         });
-
         alienRojoIzq.forEach((Rojoizq) => {
+            
             Rojoizq.dibujarse();
-            if(Rojoizq.x >= astro.x -40 ){
+            if (Rojoizq.x >= astro.x -40 ){
                 alienRojoIzq.splice(0,1)
-                astro.vida -=25
-                astro.score +=50
+                astro.vida -=35
+                score +=25 
             }
-            if(astro.vida == 0){
+                if(astro.vida == 0){
                 console.log("El juego terminó")
             }
         });
 
         aliensAmarillo.forEach((Amarillo) => {
             Amarillo.dibujarse();
-        });
+            
+            if (Amarillo.y >= astro.y && Amarillo.x >= astro.x && Amarillo.x + Amarillo.w <= astro.x + astro.w) {   
+                aliensAmarillo.splice(0,1)
+                astro.vida -=15
+            }
 
+      
+        });
+        // CREAR ELEMENTOS
         gasContainer.forEach((Gasolina) =>{
             Gasolina.dibujarse();
+            if (Gasolina.y > 900){
+                gasContainer.splice(0,1)
+            }
+            if (Gasolina.y < astro.y){
+                Gasolina +=20
+            }
+           
         });
 
-        NotasMusicales.forEach((Clave) => {
-            Clave.dibujarse()
+        MineralDeposito.forEach((oro) => {
+           oro.dibujarse(); 
+           if (oro.y > 900){
+            MineralDeposito.splice(0,1)
+        }
+            
         });
 
-       
+        ProyectilDerDeposito.forEach((ProyectilD)=>{
+            ProyectilD.dibujarse();
+        })
+
+        ProyectilDerDepositoIzq.forEach((Proyectilizq)=>{
+            Proyectilizq.dibujarse();
+        })
+
 
         crearAlien();
-        crearAlienIzq();
+        // crearAlienIzq();
         crearAlienAmarillo();
-        gasObjective()
-        }, 1000/ 30);
+        // gasObjective();
+        // generateCoin();
+    }, 1000/ 30);
 }
 
 iniciarJuego()
